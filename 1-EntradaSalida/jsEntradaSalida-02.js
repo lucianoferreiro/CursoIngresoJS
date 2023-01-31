3cc22cb37e699d2d8cf77504f766ebc006123f80
lucianoferreiro <124050744+lucianoferreiro@users.noreply.github.com>
2023-01-31 11:32:58 -0300

    Update jsEntradaSalida-02.js

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
+// Alumno: Luciano Julian Ferreiro
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
+
 {
-	alert("ok");
+	let NombrePersona = prompt("Inserte su nombre:");
+	alert("su nombre es:" NombrePersona);
 }
 
