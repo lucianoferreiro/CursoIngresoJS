987d810842ebb24f1bd2802258e9cf6f2877b22e
lucianoferreiro <124050744+lucianoferreiro@users.noreply.github.com>
2023-01-31 10:46:12 -0300

    Update jsEntradaSalida-01.js

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,9 @@
+//ALUMNO: Luciano Julian Ferreiro
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla");
 }
 
